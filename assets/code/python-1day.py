d5bc8fe641467635834ab6465533741c46762dc9
Taekwon Moon <m.taekwon.se@gmail.com>
2020-02-05 00:56:53 +0900

    blog update 1daily 1commit

---
+++ assets/code/python-1day.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# 1
+print("hello world")
+
+# 2
+if __name__=="__main__":
+  print("hello world")
+
+# 3
+a = 1; b = 2
+
+# 4 
+a = ( 1 +
+       2 + 
+    3 + 
+       4)
+print(a)
+
+# 5
+a = ( 1 +\
+       2 + \
+    3 + \
+       4)
+print(a)
