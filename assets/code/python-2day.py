e00da30d44584cf4877fc0a9711b7f9a2242151e
Taekwon Moon <m.taekwon.se@gmail.com>
2020-02-09 21:19:59 +0900

    daily update 3

--- assets/code/python-2day.py
+++ assets/code/python-2day.py
@@ -5,11 +5,35 @@ friend = 1
 Friend = 10
 print (friend)
 print (Friend)
-
 # 2
 year = 2020
 month = 2
 print (year, month)
-
 # 3
-0o10
+print ('string')
+print ("string")
+print ("This is 'string'")
+print (type('string'))
+# 4
+print("""아무것도 얻지 못한다 "그것"이 무엇이든.""")
+# 5
+print("\t탭\n다음줄")
+# 6
+print(r"\t탭\n다음줄")
+# 7
+print('py' + 'thon')
+print('py' * 3)
+# 8
+a = "python"
+print(a)
+print(a[0])
+print(a[5])
+# 9
+print(a[0:1])
+print(a[1:4])
+print(a[:2])
+print(a[-2:])
+# 10
+print(str(3.14))
+print(int("49"))
+print(float(23))
