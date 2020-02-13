7a6f6975693aad48b156e54eff782111b13ee40d
Taekwon Moon <m.taekwon.se@gmail.com>
2020-02-13 23:56:36 +0900

    daily blog update 5

---
+++ assets/code/python-4day.py
@@ -0,0 +1,30 @@
+# 1
+x = 1
+y = 2
+print(x)
+print(y)
+z = "안녕"
+print(z)
+# 2
+x = 1
+y = 2
+z = 1.2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y) #제곱
+print(x % y) #나머지
+# 3
+x = "hello"
+y = 'bye'
+z = """
+이것은
+혁신이다
+"""
+print ( x + y)
+# 4
+x = 4 # 숫자 타입
+y = "4" #문자열 타입
+print((str(x)) + y)
+print(x + (int(y)))
